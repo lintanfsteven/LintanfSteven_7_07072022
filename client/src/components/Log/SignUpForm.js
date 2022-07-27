94b9fe5a66fa3af58f551bd10f355cc7f4790c6c
StevenL <lintanf.steven@gmail.com>
2022-07-28 01:54:23 +0200

    started to work on SignUpForm

--- client/src/components/Log/SignUpForm.js
+++ client/src/components/Log/SignUpForm.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const SignUpForm = () => {
-  return <div>inscription</div>;
+  const [pseudo, setPseudo] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+  const [controlPassword, setControlPassword] = useState("");
+
+  const handleRegister = async (e) => {};
+  return (
+    <form action="" onSubmit={handleRegister} id="sign-up-form">
+      <label htmlFor="pseudo">Pseudo</label>
+      <br />
+      <input
+        type="text"
+        name="pseudo"
+        id="pseudo"
+        onChange={(e) => setPseudo(e.target.value)}
+        value="pseudo"
+      />
+      <input type="submit" value="Valider inscription" />
+    </form>
+  );
 };
 
 export default SignUpForm;
