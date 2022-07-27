ae678c9dc188b54a7ef9817585ebc25f285f78dc
StevenL <lintanf.steven@gmail.com>
2022-07-27 10:59:14 +0200

    created editCommentPost and deleteCommentPost

--- controllers/post.js
+++ controllers/post.js
@@ -6,7 +6,7 @@ exports.readPost = (req, res, next) => {
   PostModel.find((err, docs) => {
     if (!err) res.send(docs);
     else console.log("Error to get data :" + err);
-  });
+  }).sort({ createdAt: -1 });
 };
 
 exports.createPost = async (req, res, next) => {
@@ -150,7 +150,22 @@ exports.editCommentPost = (req, res, next) => {
     return res.status(400).send("ID unknown : " + req.params.id);
 
   try {
-  } catch (err) {}
+    return PostModel.findById(req.params.id, (err, docs) => {
+      const theComment = docs.comments.find((comment) => {
+        comment._id.equals(req.body.commentId);
+      });
+
+      if (!theComment) return res.status(404).send("Comment not found !");
+      theComment.text = req.body.text;
+
+      return docs.save((err) => {
+        if (!err) return res.status(200).send(docs);
+        return res.status(500).send(err);
+      });
+    });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
 };
 
 exports.deleteCommentPost = (req, res, next) => {
@@ -158,5 +173,22 @@ exports.deleteCommentPost = (req, res, next) => {
     return res.status(400).send("ID unknown : " + req.params.id);
 
   try {
-  } catch (err) {}
+    return PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: {
+          comments: {
+            _id: req.body.commentId,
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
 };
