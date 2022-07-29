0bed4f80be76fe9c3fe0527558699bf931e24d61
StevenL <lintanf.steven@gmail.com>
2022-07-29 15:35:56 +0200

    uploadPicture created

--- client/src/components/Profil/UploadImg.js
+++ client/src/components/Profil/UploadImg.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { uploadPicture } from "../../actions/user";
 
 const UploadImg = () => {
   const [file, setFile] = useState();
   const dispatch = useDispatch();
   const userData = useSelector((state) => state.userReducer);
 
-  const handlePicture = () => {};
+  const handlePicture = (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.append("name", userData.pseudo);
+    data.append("userId", userData._id);
+    data.append("file", file);
+
+    dispatch(uploadPicture(data, userData._id));
+  };
   return (
     <form action="" onSubmit={handlePicture} className="upload-pic">
       <label htmlFor="file">Changer d'image</label>
