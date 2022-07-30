cfaf6b02141e25080f965a3b72c34df9317e9480
StevenL <lintanf.steven@gmail.com>
2022-07-30 16:03:15 +0200

    created post section

--- client/src/components/Post/Card.js
+++ client/src/components/Post/Card.js
@@ -1,11 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { dateParser, isEmpty } from "../Utils";
 
 const Card = ({ post }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const usersData = useSelector((state) => state.usersReducer);
+  const userData = useSelector((state) => state.userReducer);
+
+  useEffect(() => {
+    !isEmpty(usersData[0]) && setIsLoading(false);
+  }, [usersData]);
 
   return (
     <li className="card-container" key={post._id}>
-      {isLoading ? <i className="fas fa-spinner fa-spin"></i> : <h2>test</h2>}
+      {isLoading ? (
+        <i className="fas fa-spinner fa-spin"></i>
+      ) : (
+        <>
+          <div className="card-left">
+            <img
+              src={
+                !isEmpty(usersData[0]) &&
+                usersData
+                  .map((user) => {
+                    if (user._id === post.posterId) return user.picture;
+                  })
+                  .join("")
+              }
+              alt="poster"
+            />
+          </div>
+          <div className="card-right">
+            <div className="card-header">
+              <div className="pseudo">
+                <h3>
+                  {!isEmpty(usersData[0]) &&
+                    usersData.map((user) => {
+                      if (user._id === post.posterId) return user.pseudo;
+                    })}
+                </h3>
+              </div>
+              <span>{dateParser(post.createdAt)}</span>
+            </div>
+            <p>{post.message}</p>
+            {post.picture && (
+              <img src={post.picture} alt="card" className="card-pic" />
+            )}
+            <div className="card-footer">
+              <div className="comment-icon">
+                <img src="./img/icons/message1.svg" alt="comment" />
+                <span>{post.comment.length}</span>
+              </div>
+              <h6>Like button</h6>
+              <img src="./img/icons/share.svg" alt="share" />
+            </div>
+          </div>
+        </>
+      )}
     </li>
   );
 };
